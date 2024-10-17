a2d0aaacb0fb7b3f6276802d83d6e071666b134d
skiparb <skiparb@gmail.com>
2024-10-17 09:29:11 +0200

    kodzik 17.10.24

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from contextlib import nullcontext
 
 from PyQt6.QtWidgets import QDialog, QApplication
 
@@ -9,9 +10,37 @@ class MyForm(QDialog):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.sButton.toggled.connect(self.calculate)
+        self.ui.mButton.toggled.connect(self.calculate)
+        self.ui.lButton.toggled.connect(self.calculate)
+        self.ui.xlButton.toggled.connect(self.calculate)
+        self.ui.kartaButton.toggled.connect(self.calculate)
+        self.ui.blikButton.toggled.connect(self.calculate)
+        self.ui.odbiorButton.toggled.connect(self.calculate)
         self.show()
 
+    def calculate(self):
+        rozmiar = "nie wybrano"
+        metodaPlatnosci = "nie wybrano"
+        if self.ui.sButton.isChecked():
+            rozmiar = "S"
+        elif self.ui.mButton.isChecked():
+            rozmiar = "M"
+        elif self.ui.lButton.isChecked():
+            rozmiar = "L"
+        else:
+            rozmiar = "XL"
+
+        if self.ui.kartaButton.isChecked():
+            metodaPlatnosci = "Karta"
+        if self.ui.blikButton.isChecked():
+            metodaPlatnosci = "BLIK"
+        if self.ui.odbiorButton.isChecked():
+            metodaPlatnosci = "Płatność przy odbiorze"
+
+        self.ui.outputLabel.setText(f"Rozmiar: {rozmiar}, Metoda płatności: {metodaPlatnosci}")
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyForm()
     sys.exit(app.exec())
+
